ff50f6622743b90737c66edf031a57543214a808
devdhar <yash.raj04@gmail.com>
2017-04-22 19:29:02 +0530

    Go Jek
    Package added

--- app/src/main/java/com/android/gojek/view/ContactListActivity.java
+++ app/src/main/java/com/android/gojek/view/ContactListActivity.java
@@ -1,4 +1,4 @@
-package com.android.gojek;
+package com.android.gojek.view;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.android.gojek.R;
 
 public class ContactListActivity extends AppCompatActivity {
 
